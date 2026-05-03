13750583d0559912addd62fd8f5ad289012d8942
Gilberto Maia <gilbertomaia258@gmail.com>
2026-05-03 00:40:41 -0300

    Primeiro commit - Conta Azul API

--- push_github.bat
+++ push_github.bat
@@ -1,39 +1,14 @@
 @echo off
 
-echo ================================
-echo Inicializando repositório...
-echo ================================
-
 git init
-
-echo ================================
-echo Adicionando arquivos...
-echo ================================
-
 git add .
-
-echo ================================
-echo Commitando...
-echo ================================
-
 git commit -m "Primeiro commit - Conta Azul API"
-
-echo ================================
-echo Conectando ao GitHub...
-echo ================================
 
 git branch -M main
 
+git remote remove origin 2>nul
 git remote add origin https://github.com/bebetin258/contaazul-api.git
 
-echo ================================
-echo Enviando para o GitHub...
-echo ================================
-
 git push -u origin main
-
-echo ================================
-echo FINALIZADO 🚀
-echo ================================
 
 pause
