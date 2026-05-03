74190cd8e8ae31af4ebb140302fadb15063eba2a
Gilberto Maia <gilbertomaia258@gmail.com>
2026-05-03 00:58:05 -0300

    update automatico 03/05/2026  0:58:05,35

--- push_github.bat
+++ push_github.bat
@@ -1,44 +1,19 @@
 @echo off
 
-echo ================================
-echo Sincronizando repositorio...
-echo ================================
+echo Limpando locks antigos...
+del /f /q .git\*.lock 2>nul
 
-git init
-
+echo Commitando...
 git add .
 git commit -m "update automatico %date% %time%"
 
-echo ================================
-echo Ajustando branch...
-echo ================================
-
 git branch -M main
-
-echo ================================
-echo Configurando remoto...
-echo ================================
 
 git remote remove origin 2>nul
 git remote add origin https://github.com/bebetin258/contaazul-api.git
 
-echo ================================
-echo Tentando push normal...
-echo ================================
+echo Enviando...
+git push -u origin main --force
 
-git pull origin main --allow-unrelated-histories
-
-git push -u origin main
-
-IF %ERRORLEVEL% NEQ 0 (
-    echo ================================
-    echo ERRO detectado, forçando envio...
-    echo ================================
-    git push -u origin main --force
-)
-
-echo ================================
 echo FINALIZADO 🚀
-echo ================================
-
 pause
